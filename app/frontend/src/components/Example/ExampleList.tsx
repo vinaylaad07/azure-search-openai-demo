7229a949f4e3c9b09d2e87cc8892029b4490a7d4
vinay.laad <vinay.vilas.laad@accenture.com>
2024-02-16 09:49:52 +0000

    updated example list

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -6,7 +6,7 @@ const DEFAULT_EXAMPLES: string[] = [
     "What is included in my Northwind Health Plus plan that is not in standard?",
     "What happens in a performance review for Eirgrid employees?",
     "What does a Product Manager do in Eirgrid?",
-    "What is the address for Vinay Laad?"
+    "What is the climate action plan for 2023?"
 ];
 
 const GPT4V_EXAMPLES: string[] = [
