9dee23eb737998c174ceb12a2da325ddb49d9e92
vinay.laad <vinay.vilas.laad@accenture.com>
2024-02-16 02:24:17 +0000

    updated central logo

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import { Checkbox, Panel, DefaultButton, TextField, SpinButton } from "@fluentui/react";
-import SparkleFilled from "../../assets/eirgrid.svg";
+import eirgrid from "../../assets/eirgrid.svg";
 import readNDJSONStream from "ndjson-readablestream";
 
 import styles from "./Chat.module.css";
@@ -269,7 +269,7 @@ const Chat = () => {
                 <div className={styles.chatContainer}>
                     {!lastQuestionRef.current ? (
                         <div className={styles.chatEmptyState}>
-                            <SparkleFilled fontSize={"120px"} primaryFill={"rgba(115, 118, 225, 1)"} aria-hidden="true" aria-label="Chat logo" />
+                            <img src={eirgrid} alt="eirgrid logo" aria-label="Link to Eirgrid" width="50px" height="50px" className={styles.eirgridLogo} />
                             <h1 className={styles.chatEmptyStateTitle}>Hi Eirgrid, Chat with your data</h1>
                             <h2 className={styles.chatEmptyStateSubtitle}>Ask anything or try an example</h2>
                             <ExampleList onExampleClicked={onExampleClicked} useGPT4V={useGPT4V} />
